1b25c278933467775d403c4a166d8d65b46876c0
Elias Delgado <evdelgado99@gmail.com>
2020-09-07 23:21:48 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -234,7 +234,7 @@ def big_shoe_rebounds
     
     team_data[:players].each do |player|
       
-      return player[:shoe].max_by{|v|}
+      return player[:shoe].max_by if player[:shoe] > player[:shoe]
       
     end
     
